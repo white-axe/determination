95c9f6361af714a929667d44f6776e7b74b81bc3
刘皓 <whiteaxe@tuta.io>
2024-05-12 13:43:48 -0400

    Increase robustness of .determination-ardour-isolate.lua
    Instead of only disabling the instrument plugin in each track, it'll now
    disable every plugin in each track as well as the track itself (in case
    one of the tracks is an audio track).

--- stuff/bin/.determination-ardour-isolate.lua
+++ stuff/bin/.determination-ardour-isolate.lua
@@ -18,9 +18,30 @@ assert(session)
 
 i = math.tointeger(os.getenv("DETERMINATION_ARDOUR_TRACK"))
 
+-- Make sure disabling a track in a group doesn't also disable the other tracks
+-- in the group
+for group in session:route_groups():iter() do
+  group:set_route_active(false)
+end
+
 for j, track in ipairs(session:get_tracks():table()) do
   if j ~= i then
-    track:remove_processor(track:the_instrument(), nil, true)
+    -- Disable every track except for track i
+    track:set_active(false, nil)
+    -- Disable all the plugins in every track except for track i as well,
+    -- otherwise Ardour will still load the plugins, and even doing that can
+    -- cause exported audio to change!
+    k = 0
+    while true do
+      processor = track:nth_processor(k)
+      if processor:isnil() then
+        break
+      elseif processor:to_plugininsert():isnil() then
+        k = k + 1
+      else
+        assert(track:remove_processor(processor, nil, true) == 0)
+      end
+    end
   end
 end
 
