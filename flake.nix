68fdbf17eee90313186738898bccfee13f619cd1
刘皓 <whiteaxe@tuta.io>
2024-05-15 14:36:43 -0400

    Encode image layers in pax tar format instead of GNU tar format

--- flake.nix
+++ flake.nix
@@ -77,26 +77,61 @@
         };
         nativeBuildInputs = [
           pkgs.jq
+          pkgs.libarchive
           pkgs.skopeo
+          pkgs.zstd
         ];
         unpackPhase = ":";
         buildPhase = ''
-          skopeo --debug --insecure-policy --tmpdir="$TMPDIR" copy -f oci --dest-compress-format zstd --dest-compress-level 20 docker-archive:"$src" oci-archive:image.tar
+          skopeo --debug --insecure-policy --tmpdir="$TMPDIR" copy -f oci --dest-compress-format zstd docker-archive:"$src" oci-archive:image.tar
           mkdir image
           tar -C image -xf image.tar
           rm image.tar
           cd image
-          digests=$(jq -er '.manifests[].digest' < index.json)
+          manifest_digests=$(jq -er '.manifests[].digest' < index.json)
           i=0
-          for digest in $digests; do
-            digest=''${digest#*:}
+          for manifest_digest in $manifest_digests; do  # Annotate every manifest
+            manifest_digest=''${manifest_digest#*:}
             cd blobs/sha256
-            jq -c ".annotations += { \"org.opencontainers.image.description\": \"Deterministic rendering environment for white-axe's music and art\", \"org.opencontainers.image.licenses\": \"GPL-3.0-only\", \"org.opencontainers.image.source\": \"https://github.com/white-axe/determination\", \"org.opencontainers.image.title\": \"Determination\" }" < $digest > $digest.out && mv $digest.out $digest
-            new_size=$(wc -c < $digest)
-            new_digest=$(sha256sum $digest | awk '{print $1}')
-            mv $digest $new_digest
+            mv $manifest_digest manifest.json
+            jq -c ".annotations += { \"org.opencontainers.image.description\": \"Deterministic rendering environment for white-axe's music and art\", \"org.opencontainers.image.licenses\": \"GPL-3.0-only\", \"org.opencontainers.image.source\": \"https://github.com/white-axe/determination\", \"org.opencontainers.image.title\": \"Determination\" }" < manifest.json > manifest.json.out && mv manifest.json.out manifest.json
+            config_digest=$(jq -er '.config.digest' < manifest.json)
+            config_digest=''${config_digest#*:}
+            mv $config_digest config.json
+            layer_digests=$(jq -er ".layers | map(select(.mediaType == \"application/vnd.oci.image.layer.v1.tar+zstd\"))[].digest" < manifest.json)
+            j=0
+            for layer_digest in $layer_digests; do  # Convert every layer from GNU tar format to pax tar format
+              echo "Decompressing layer $j from manifest $i..."
+              layer_digest=''${layer_digest#*:}
+              mv $layer_digest old-layer.tar.zst
+              zstd -d old-layer.tar.zst
+              rm old-layer.tar.zst
+              echo "Re-encoding layer $j from manifest $i..."
+              bsdtar --format=pax -cf layer.tar @old-layer.tar
+              new_diff_id=$(sha256sum layer.tar | awk '{print $1}')
+              rm old-layer.tar
+              echo "Compressing layer $j from manifest $i..."
+              zstd --ultra -20 layer.tar
+              rm layer.tar
+              echo "Updating config for manifest $i..."
+              jq -c ".rootfs.diff_ids[$j] = \"sha256:$new_diff_id\"" < config.json > config.json.out && mv config.json.out config.json
+              new_config_size=$(wc -c < config.json)
+              new_config_digest=$(sha256sum config.json | awk '{print $1}')
+              mv config.json $new_config_digest
+              echo "Updating manifest $i..."
+              jq -c ".config += { digest: \"sha256:$new_config_digest\", size: $new_config_size }" < manifest.json > manifest.json.out && mv manifest.json.out manifest.json
+              new_layer_size=$(wc -c < layer.tar.zst)
+              new_layer_digest=$(sha256sum layer.tar.zst | awk '{print $1}')
+              mv layer.tar.zst $new_layer_digest
+              jq -c ".layers[$j] += { digest: \"sha256:$new_layer_digest\", size: $new_layer_size }" < manifest.json > manifest.json.out && mv manifest.json.out manifest.json
+              ((++j))
+            done
+            echo "Updating index.json..."
+            new_manifest_size=$(wc -c < manifest.json)
+            new_manifest_digest=$(sha256sum manifest.json | awk '{print $1}')
+            mv manifest.json $new_manifest_digest
             cd ../..
-            jq -c ".manifests[$i] += { digest: \"sha256:$new_digest\", size: $new_size }" < index.json > index.json.out && mv index.json.out index.json
+            jq -c ".manifests[$i] += { digest: \"sha256:$new_manifest_digest\", size: $new_manifest_size }" < index.json > index.json.out && mv index.json.out index.json
             ((++i))
           done
           cd ..
