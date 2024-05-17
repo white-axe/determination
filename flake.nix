7df877727345b55e946363072ee2edec9a8a59b8
刘皓 <whiteaxe@tuta.io>
2024-05-17 13:28:38 -0400

    Stop using `pkgs.dockerTools.buildImage`
    Instead of using `pkgs.dockerTools.buildImage` to build a Docker image
    and then converting it to an OCI image, we can just build an OCI image
    directly.

--- flake.nix
+++ flake.nix
@@ -20,8 +20,9 @@
       pkgsUnstable = import unstable { inherit system; };
       config = import ./config.nix;
       formatter = pkgsUnstable.nixfmt-rfc-style;
+      name = "determination";
       raw = pkgs.buildEnv {
-        name = "determination";
+        inherit name;
         paths =
           [
             ./stuff
@@ -53,100 +54,118 @@
             "/root/.config/ardour8"
           ];
       };
+      baseJson = pkgs.writeText "${name}-dummy-config.json" "{}";
+      layer = pkgs.dockerTools.mkRootLayer {
+        inherit name baseJson;
+        copyToRoot = raw;
+        runAsRoot =
+          pkgs.dockerTools.shadowSetup
+          + pkgs.lib.optionalString (!config.krita) ''
+            rm -f /bin/determination-krita-*
+            rm -f /bin/.determination-krita-*
+          ''
+          + pkgs.lib.optionalString (!config.ardour) ''
+            rm -f /bin/determination-ardour-*
+            rm -f /bin/.determination-ardour-*
+          '';
+      };
       container =
-        pkgs.runCommand "container-image-determination.tar"
+        pkgs.runCommand "container-image-${name}.tar"
           {
-            src = pkgs.dockerTools.buildImage {
-              name = "determination";
-              tag = "latest";
-              copyToRoot = raw;
-              runAsRoot =
-                pkgs.dockerTools.shadowSetup
-                + pkgs.lib.optionalString (!config.krita) ''
-                  rm -f /bin/determination-krita-*
-                  rm -f /bin/.determination-krita-*
-                ''
-                + pkgs.lib.optionalString (!config.ardour) ''
-                  rm -f /bin/determination-ardour-*
-                  rm -f /bin/.determination-ardour-*
-                '';
-              #compressor = "zstd";
-              config = {
-                Cmd = [ "/bin/bash" ];
-              };
-            };
             nativeBuildInputs = [
               pkgs.jq
               pkgs.libarchive
-              pkgs.skopeo
               pkgs.zstd
             ];
+            closure = pkgs.writeReferencesToFile layer;
+            config = pkgs.writeText "${name}-config.json" (builtins.toJSON { Cmd = [ "/bin/bash" ]; });
+            annotations = pkgs.writeText "${name}-annotations.json" (
+              builtins.toJSON {
+                "org.opencontainers.image.source" = "https://github.com/white-axe/determination";
+                "org.opencontainers.image.licenses" = "GPL-3.0-only";
+                "org.opencontainers.image.title" = "Determination";
+                "org.opencontainers.image.description" = "Deterministic rendering environment for white-axe's music and art";
+              }
+            );
           }
           ''
-            skopeo --debug --insecure-policy --tmpdir="$TMPDIR" copy -f oci --dest-compress-format zstd docker-archive:"$src" oci-archive:image.tar
-            mkdir image
-            tar -C image -xf image.tar
-            rm image.tar
-            cd image
-            unset layer_digest_map; declare -A layer_digest_map
-            manifest_digests=$(jq -er '.manifests[].digest' < index.json)
-            i=0
-            for manifest_digest in $manifest_digests; do  # Annotate every manifest
-              manifest_digest=''${manifest_digest#*:}
-              cd blobs/sha256
-              mv $manifest_digest manifest.json
-              jq -c ".annotations += { \"org.opencontainers.image.description\": \"Deterministic rendering environment for white-axe's music and art\", \"org.opencontainers.image.licenses\": \"GPL-3.0-only\", \"org.opencontainers.image.source\": \"https://github.com/white-axe/determination\", \"org.opencontainers.image.title\": \"Determination\" }" < manifest.json > manifest.json.out && mv manifest.json.out manifest.json
-              config_digest=$(jq -er '.config.digest' < manifest.json)
-              config_digest=''${config_digest#*:}
-              mv $config_digest config.json
-              layer_digests=$(jq -er ".layers | map(select(.mediaType == \"application/vnd.oci.image.layer.v1.tar+zstd\"))[].digest" < manifest.json)
-              j=0
-              for layer_digest in $layer_digests; do  # Convert every layer from GNU tar format to pax tar format
-                layer_digest=''${layer_digest#*:}
-                if [ -z "''${layer_digest_map[$layer_digest]}" ]; then
-                  echo "Decompressing layer $j from manifest $i..."
-                  mv $layer_digest old-layer.tar.zst
-                  zstd -d old-layer.tar.zst
-                  rm old-layer.tar.zst
-                  echo "Re-encoding layer $j from manifest $i..."
-                  bsdtar --format=pax -cf layer.tar @old-layer.tar
-                  rm old-layer.tar
-                  echo "Updating layer info for layer $j in config for manifest $i..."
-                  new_diff_id=$(sha256sum layer.tar | awk '{print $1}')
-                  jq -c ".rootfs.diff_ids[$j] = \"sha256:$new_diff_id\"" < config.json > config.json.out && mv config.json.out config.json
-                  echo "Compressing layer $j from manifest $i..."
-                  zstd -T$NIX_BUILD_CORES --ultra -20 layer.tar
-                  rm layer.tar
-                  echo "Updating layer info for layer $j in manifest $i..."
-                  new_layer_size=$(wc -c < layer.tar.zst)
-                  new_layer_digest=$(sha256sum layer.tar.zst | awk '{print $1}')
-                  mv layer.tar.zst $new_layer_digest
-                  layer_digest_map[$layer_digest]="$new_layer_digest-$new_layer_size"
-                else
-                  echo "Updating layer info for layer $j in manifest $i..."
-                  new_layer_size=''${layer_digest_map[$layer_digest]#*-}
-                  new_layer_digest=''${layer_digest_map[$layer_digest]%-*}
+            ls_tar() {
+              for f in $(tar -tf $1 | xargs realpath -ms --relative-to=.); do
+                if [[ "$f" != "." ]]; then
+                  echo "/$f"
                 fi
-                jq -c ".layers[$j] += { digest: \"sha256:$new_layer_digest\", size: $new_layer_size }" < manifest.json > manifest.json.out && mv manifest.json.out manifest.json
-                ((++j))
               done
-              echo "Updating config info in manifest $i..."
-              new_config_size=$(wc -c < config.json)
-              new_config_digest=$(sha256sum config.json | awk '{print $1}')
-              mv config.json $new_config_digest
-              jq -c ".config += { digest: \"sha256:$new_config_digest\", size: $new_config_size }" < manifest.json > manifest.json.out && mv manifest.json.out manifest.json
-              echo "Updating index.json..."
-              new_manifest_size=$(wc -c < manifest.json)
-              new_manifest_digest=$(sha256sum manifest.json | awk '{print $1}')
-              mv manifest.json $new_manifest_digest
-              cd ../..
-              jq -c ".manifests[$i] += { digest: \"sha256:$new_manifest_digest\", size: $new_manifest_size }" < index.json > index.json.out && mv index.json.out index.json
-              ((++i))
+            }
+
+            mkdir image
+            cd image
+            echo "{\"imageLayoutVersion\":\"1.0.0\"}" > oci-layout
+            mkdir -p blobs/sha256
+            cd blobs/sha256
+
+            echo 'Converting layer to pax format...'
+            bsdtar --format=pax -cf layer.tar @"${layer}"/layer.tar
+
+            echo 'Copying layer closure to layer...'
+            ls_tar layer.tar >> baseFiles
+            for dep in $(cat "$closure"); do
+              find "$dep" >> layerFiles
             done
+            mkdir nix
+            mkdir nix/store
+            chmod -R 555 nix/
+            echo './nix' >> layerFiles
+            echo './nix/store' >> layerFiles
+            comm <(sort -n baseFiles|uniq) <(sort -n layerFiles|uniq|grep -v ${layer}) -1 -3 > newFiles
+            LC_ALL=C tar --hard-dereference --sort=name --format=posix --mtime="@$SOURCE_DATE_EPOCH"  --owner=0 --group=0 --numeric-owner --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime --no-recursion --verbatim-files-from --files-from newFiles -rpf layer.tar
+            chmod -R 755 nix/
+            rm -r nix/
+            rm newFiles
+            rm layerFiles
+            rm baseFiles
+            layer_diff_id=$(sha256sum layer.tar | awk '{print $1}')
+            echo "Layer DiffID: sha256:$layer_diff_id"
+            echo "Layer decompressed size: $(wc -c < layer.tar)"
+
+            echo "Compressing layer with \`zstd -T$NIX_BUILD_CORES --ultra -20 layer.tar\`..."
+            zstd -T$NIX_BUILD_CORES --ultra -20 layer.tar
+            rm layer.tar
+            layer_digest=$(sha256sum layer.tar.zst | awk '{print $1}')
+            echo "Layer digest: sha256:$layer_digest"
+            layer_size=$(wc -c < layer.tar.zst)
+            echo "Layer size: $layer_size"
+            mv layer.tar.zst $layer_digest
+
+            echo 'Creating config...'
+            jq -c "{ architecture: \"amd64\", os: \"linux\", config: ., rootfs: { type: \"layers\", diff_ids: [\"sha256:$layer_diff_id\"] } }" "$config" > config.json
+            cat config.json
+            config_digest=$(sha256sum config.json | awk '{print $1}')
+            echo "Config digest: sha256:$config_digest"
+            config_size=$(wc -c < config.json)
+            echo "Config size: $config_size"
+            mv config.json $config_digest
+
+            echo 'Creating manifest...'
+            jq -c "{ schemaVersion: 2, mediaType: \"application/vnd.oci.image.manifest.v1+json\", config: { mediaType: \"application/vnd.oci.image.config.v1+json\", digest: \"sha256:$config_digest\", size: $config_size }, layers: [{ mediaType: \"application/vnd.oci.image.layer.v1.tar+zstd\", digest: \"sha256:$layer_digest\", size: $layer_size }], annotations: . }" "$annotations" > manifest.json
+            cat manifest.json
+            manifest_digest=$(sha256sum manifest.json | awk '{print $1}')
+            echo "Manifest digest: sha256:$manifest_digest"
+            manifest_size=$(wc -c < manifest.json)
+            echo "Manifest size: $manifest_size"
+            mv manifest.json $manifest_digest
+
+            cd ../..
+
+            echo 'Creating index...'
+            jq -c "{ schemaVersion: 2, manifests: [{ mediaType: \"application/vnd.oci.image.manifest.v1+json\", digest: \"sha256:$manifest_digest\", size: $manifest_size }] }" <(echo "{}") > index.json
+            cat index.json
+
+            echo 'Archiving...'
             LC_ALL=C tar --sort=name --format=posix --mtime="@$SOURCE_DATE_EPOCH" --owner=0 --group=0 --numeric-owner --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime -cf "$out" *
           '';
     in
     {
+      formatter.${system} = formatter;
       packages.${system} = {
         default = container;
         inherit container formatter raw;
@@ -154,6 +173,5 @@
         tar = pkgs.gnutar;
         zstd = pkgs.zstd;
       };
-      formatter.${system} = formatter;
     };
 }
